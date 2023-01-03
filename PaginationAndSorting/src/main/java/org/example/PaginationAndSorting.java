012819f4b5025713ee3c93358d427b60fb10e8ed
BallikuraRam <tr20@sharpgurus-usa.com>
2023-01-03 12:19:12 +0530

    initial commit

---
+++ PaginationAndSorting/src/main/java/org/example/PaginationAndSorting.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginationAndSorting {
+    public static void main(String[] args) {
+        SpringApplication.run(PaginationAndSorting.class);
+    }
+}
